186743f1f140b7e6272fd421f5d6651d75bc52b8
ritishadhikari <radhik0607@gmail.com>
2022-02-24 17:08:42 +0530

    Chenged Name Ritish

--- src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
+++ src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
@@ -45,7 +45,7 @@ class ValidatorTests {
 
 		LocaleContextHolder.setLocale(Locale.ENGLISH);
 		Person person = new Person();
-		person.setFirstName("Not Empty");
+		person.setFirstName("NotEmptyRitish");
 		person.setLastName("smith");
 
 		Validator validator = createValidator();
