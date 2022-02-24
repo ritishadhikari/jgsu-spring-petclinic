e3a8711d6e4464e9527d07137aa7e0ff96f8dff2
ritishadhikari <radhik0607@gmail.com>
2022-02-24 17:11:26 +0530

    Changed Name Ritish Adhikari

--- src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
+++ src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
@@ -45,7 +45,7 @@ class ValidatorTests {
 
 		LocaleContextHolder.setLocale(Locale.ENGLISH);
 		Person person = new Person();
-		person.setFirstName("NotEmptyRitish");
+		person.setFirstName("NotEmptyRitishAdhikari");
 		person.setLastName("smith");
 
 		Validator validator = createValidator();
